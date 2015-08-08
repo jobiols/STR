b7e062daae20a9517fa452e271d02f56cc36d429
jobiols <jorge.obiols@gmail.com>
2015-08-07 22:48:12 -0300

    pep8 fix

--- oerp_no_phoning_home/__init__.py
+++ oerp_no_phoning_home/__init__.py
@@ -1,4 +1,4 @@
-#/#############################################################################
+# #############################################################################
 #
 #    BizzAppDev
 #    Copyright (C) 2004-TODAY bizzappdev(<http://www.bizzappdev.com>).
@@ -16,6 +16,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#/#############################################################################
+# #############################################################################
 import mail
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
