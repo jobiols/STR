6347587b14217142b0732f1b98fc96c4958b2938
jobiols <jorge.obiols@gmail.com>
2015-09-25 18:40:38 -0300

    [IMP] Mejoras en documentación

--- curso/__openerp__.py
+++ curso/__openerp__.py
@@ -25,19 +25,24 @@
     'category': 'Tools',
     'summary': 'Cursos, Inscripciones, Reservas etc.',
     'description': """
-Manejo de Cursos en una Academia.
-=================================
-
-El módulo curso le ayuda a manejar y organizar en forma eficiente los cursos de una academia.
+Manejo de Cursos en una Academia
+================================
+El módulo curso le ayuda a manejar y organizar en forma eficiente los cursos de una \
+academia.
 
 Puntos Clave
 ------------
-* Definicion de todos los cursos con el estado, Proximo, Cursando, terminado, cancelado
-* Estados de los alumnos, Interesado, Señado, Cursando, Terminado, Cancelado
-* Historia de las cursadas registradas en la ficha del alumno
-* Generación automática de la facturación
-* Reporte diario de asistentes con detalle de cuotas adeudadas e información faltante en la ficha
-* Vista de calendario de las clases
+- Definición de los cursos como Productos (servicios)
+- Definicion las instancias de los cursos, cada vez que se vuelve a dictar un curso \
+se autoincrementa en nro de instancia.
+- Estados de los cursos Borrador, Cursando, Terminado, Cancelado
+- Estados de los alumnos inscriptos en los cursos, Interesado, Señado, Cursando, \
+Terminado, Cancelado
+- Historia de las cursadas registradas en la ficha del alumno
+- Generación automática de la facturación
+- Reporte diario de asistentes con detalle de cuotas adeudadas e información faltante \
+en la ficha del alumno
+- Vista de calendario de las clases
 
 """,
     'author': 'jeo software',
@@ -46,7 +51,7 @@ Puntos Clave
     'data': [
         'security/curso_security.xml',
         'security/ir.model.access.csv',
-        #        'wizard/curso_confirm_view.xml',
+        # 'wizard/curso_confirm_view.xml',
 
         'wizard/add_registration_view.xml',
 
