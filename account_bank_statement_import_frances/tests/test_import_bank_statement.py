750287a7d7adac2df7f6e56f38f9ce099cb242e4
jobiols <jorge.obiols@gmail.com>
2015-12-31 10:44:31 -0300

    [FIX]  test

--- account_bank_statement_import_frances/tests/test_import_bank_statement.py
+++ account_bank_statement_import_frances/tests/test_import_bank_statement.py
@@ -19,14 +19,13 @@ class TestQifFile(TransactionCase):
         qif_file_path = get_module_resource(
             'account_bank_statement_import_frances',
             'test_qif_file', 'test_frances.csv')
-
-        print 'file path >>>>>>>>>>>>>>>>>>>>', qif_file_path
         qif_file = open(qif_file_path, 'rb').read().encode('base64')
-        print 'file opened >>>>>>>>>>>>>>>>>>>', qif_file
-
+        print '>>>>>>>>>>>>>>>>>>>>>> bank statement import'
         bank_statement_improt = self.statement_import_model.with_context(
             journal_id=self.ref('account.bank_journal')).create(
             dict(data_file=qif_file))
+        print '>>>>>>>>>>>>>>>>>>>>>> bank statement import'
+
         bank_statement_improt.import_file()
         bank_statement = self.statement_line_model.search(
             [('name', '=', 'TRANSFERENCIA XX0000001266940')], limit=1)[0].statement_id
