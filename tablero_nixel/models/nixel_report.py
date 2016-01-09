84560e73d99034258bf948fcce1f271c7cdd6868
jobiols <jorge.obiols@gmail.com>
2016-01-09 11:38:55 -0300

    [FIX]  desarrollando reporte

--- tablero_nixel/models/nixel_report.py
+++ tablero_nixel/models/nixel_report.py
@@ -22,18 +22,74 @@ import time
 from openerp.osv import osv
 from openerp.report import report_sxw
 
-class nixel_report_demo(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(nixel_report_demo, self).__init__(cr, uid, name, context=context)
+PROVEEDORES = 79
+DEUDORES_POR_VENTAS = 8
+GASTOS = 155
+
+
+class nixel_report_def(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context=None):
+        super(nixel_report_def, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
+            'get_debtors': self._get_debtors,
+            'get_creditors': self._get_creditors,
+
+            'get_venta': self._get_venta,
+            'get_compra': self._get_compra,
+            'get_gastos': self._get_gastos,
         })
+
+    def _summarize_account(self, cr, uid, account_id):
+        """
+        Sumariza la cuenta account_id en elementos del elementos del diario dando
+        los créditos y débitos en un diccionario
+        """
+        accounts = self.pool['account.move.line']
+        ids = accounts.search(cr, uid, [('account_id', '=', account_id)])
+        debit = credit = 0.0
+        for account in accounts.browse(cr, uid, ids):
+            debit += account.debit
+            credit += account.credit
+        return {'debit': debit, 'credit': credit}
+
+    def _get_debtors(self):
+        lis = [
+            {'name': 'juan debe', 'amount': 50},
+            {'name': 'pepe', 'amount': 51},
+            {'name': 'maria', 'amount': 53},
+            {'name': 'pedro', 'amount': 55},
+        ]
+        return lis
+
+    def _get_creditors(self):
+        lis = [
+            {'name': 'juan le debo', 'amount': 500},
+            {'name': 'pepe', 'amount': 510},
+            {'name': 'maria', 'amount': 530},
+            {'name': 'pedro', 'amount': 550},
+        ]
+        return lis
+
+    def _get_venta(self):
+        dic = self._summarize_account(self.cr, self.uid, DEUDORES_POR_VENTAS)
+        return {'fac': dic['debit'], 'cob': dic['credit'],
+                'pen': dic['debit'] - dic['credit']}
+
+    def _get_compra(self):
+        dic = self._summarize_account(self.cr, self.uid, PROVEEDORES)
+        return {'fac': dic['credit'], 'cob': dic['debit'],
+                'pen': dic['credit'] - dic['debit']}
+
+    def _get_gastos(self):
+        dic = self._summarize_account(self.cr, self.uid, GASTOS)
+        return {'gas': dic['debit']}
 
 
 class report_nixel_class(osv.AbstractModel):
     _name = 'report.tablero_nixel.nixel_report'
     _inherit = 'report.abstract_report'
     _template = 'tablero_nixel.nixel_report'
-    _wrapped_report_class = nixel_report_demo
+    _wrapped_report_class = nixel_report_def
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
