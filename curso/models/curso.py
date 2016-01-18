d982c5ea74a0fb7522b61a4b017c77e051bd5099
jobiols <jorge.obiols@gmail.com>
2016-01-17 23:04:35 -0300

    desarrollar multiples dias y horarios de cursada #28

--- curso/models/curso.py
+++ curso/models/curso.py
@@ -549,8 +549,8 @@ class curso_curso(osv.osv):
 
     def onchange_date_begin(self, cr, uid, ids, context=None):
         print 'onchange_date_begin', cr, uid, ids, context
-        self._check_change_begin_date(cr, uid, ids, context=None)
-        return True
+        #        self._check_change_begin_date(cr, uid, ids, context=None)
+        return 'hola'
 
     def onchange_curso_product(self, cr, uid, ids, product, context=None):
         values = {}
