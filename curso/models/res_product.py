b731c2a61abfdd4876d3b9301dfd79e2e81bb2a7
jobiols <jorge.obiols@gmail.com>
2016-10-23 14:23:56 -0300

    [ADD] agregar soporte para contar caracteres en descripción de producto

--- curso/models/res_product.py
+++ curso/models/res_product.py
@@ -107,14 +107,39 @@ class product_product(models.Model):
     )
 
     woo_id = fields.Char(
-        help = u'Identifica el producto en woo'
+        help=u'Identifica el producto en woo'
     )
 
     woo_categ = fields.Many2one(
         'curso.woo.categ',
         'Categoría de producto',
-        help = u'Categoría de producto'
+        help=u'Categoría de producto'
     )
+
+    description_short_wc = fields.Char(
+        compute="_compute_short_wc"
+    )
+
+    @api.one
+    @api.depends('description')
+    def _compute_short_wc(self):
+        ix = self.description.find('<mas>')
+        if ix <> -1:
+            # existe el simbolo <mas>
+            if ix < 400:
+                self.description_short_wc = \
+                    u'Total {} caracteres antes del símbolo <mas>'.format(ix)
+            else:
+                self.description_short_wc = \
+                    u'Total {} DEMASIADOS CARACTERES ANTES DE <mas>'.format(ix)
+        else:
+            # no existe el simbolo <mas>
+            if len(self.description) < 400:
+                self.description_short_wc = \
+                    u'Total {} caracteres'.format(len(self.description))
+            else:
+                self.description_short_wc = \
+                    u'Debe incluir el simbolo <mas> para reducir la cantidad de caracteres!!'
 
     @api.one
     @api.depends('lst_price', 'standard_price')
