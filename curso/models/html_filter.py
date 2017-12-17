c1719b13c6084eb30471c12ba8b7dd6ed3a77d6d
jobiols <jorge.obiols@gmail.com>
2017-12-17 14:08:02 -0300

    [FIX] corregir etiqueta src= de la tienda

--- curso/models/html_filter.py
+++ curso/models/html_filter.py
@@ -82,12 +82,19 @@ class html_filter:
 </table>
 <table>
 <tbody>
-<tr>
-
-<td><a href="makeoverlab.mitiendanube.com"><img src="http://makeoverlab.com.ar/wp-content/uploads/2017/02/social-tienda.png" alt="sitio web" width="26" height="26" />&nbsp;Nuestra tienda virtual</a>&nbsp;</td>
-
-<td><a href="http://www.makeoverlab.com.ar"><img src="http://makeoverlab.com.ar/wp-content/uploads/2017/02/social-web.png" alt="sitio web" width="26" height="26" />&nbsp;Nuestro Sitio web</a>&nbsp;</td>
-</tr>
+    <tr>
+        <td><a href="https://makeoverlab.mitiendanube.com/">
+                <img src="http://makeoverlab.com.ar/wp-content/uploads/2017/02/social-tienda.png" alt="sitio web" width="26" height="26" />
+                &nbsp;Nuestra tienda virtual
+            </a>
+        </td>
+        <td>
+            <a href="http://www.makeoverlab.com.ar">
+                <img src="http://makeoverlab.com.ar/wp-content/uploads/2017/02/social-web.png" alt="sitio web" width="26" height="26" />
+                &nbsp;Nuestro Sitio web
+            </a>
+        </td>
+    </tr>
 </tbody>
 </table>
 
