85d7ae2f103fec7c99ecdd00faf72f5eafcf69cb
jobiols <jorge.obiols@gmail.com>
2018-01-31 12:55:28 -0300

    [FIX] bajar version postgres a 9.4

--- makeover_default/__openerp__.py
+++ makeover_default/__openerp__.py
@@ -57,7 +57,7 @@ Customización Makeover Lab
     'docker': [
         {'name': 'aeroo', 'usr': 'jobiols', 'img': 'aeroo-docs'},
         {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '8.0'},
-        {'name': 'postgres', 'usr': 'postgres', 'ver': '9.5'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '9.4'},
         {'name': 'backup', 'usr': 'jobiols', 'img': 'backup'},
     ],
 
